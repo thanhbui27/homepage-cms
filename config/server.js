5047758a2288962351eeab1ea261f99f3f3f4858
Bùi Ngọc Thành <63583108+thanhbui27@users.noreply.github.com>
2024-03-29 17:40:52 +0700

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'homepage-cms.vercel.app'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
