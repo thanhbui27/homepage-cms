4670eb274ad946a3040871287800008e18212e12
Bùi Ngọc Thành <63583108+thanhbui27@users.noreply.github.com>
2024-04-19 19:24:42 +0700

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,8 @@
+const crypto = require('crypto');
 module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
-      jwtSecret: env("JWT_SECRET"),
+      jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString('base64'),
     },
   },
 });
