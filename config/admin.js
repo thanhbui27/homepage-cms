c364296bb9a9e0d3d6182dfc60566cbb92cd602f
Bùi Ngọc Thành <63583108+thanhbui27@users.noreply.github.com>
2024-04-19 19:33:54 +0700

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,14 @@
+const crypto = require('crypto');
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT') || crypto.randomBytes(16).toString('base64'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT')  || crypto.randomBytes(16).toString('base64'),
     },
   },
 });
