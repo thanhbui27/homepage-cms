54fd80963e53659c001079f2372ace8f68bcbb49
Bùi Ngọc Thành <63583108+thanhbui27@users.noreply.github.com>
2024-04-19 19:58:20 +0700

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT') || crypto.randomBytes(16).toString('base64'),
